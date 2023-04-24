c6da1dbca728e06fbb884c2e0fc09da44eeabffc
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-04-24 22:47:52 +0600

    Update script.js

---
+++ Project 8 Form/script.js
@@ -0,0 +1,9 @@
+const labels = document.querySelector('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span> ${letter} </span>`)
+        .join('')
+    
+})

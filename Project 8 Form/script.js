11d789f96317c2c7d7195a14f043f0919444e91b
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-04-24 23:25:03 +0600

    Update script.js

--- Project 8 Form/script.js
+++ Project 8 Form/script.js
@@ -3,7 +3,7 @@ const labels = document.querySelectorAll('.form-control label')
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
-        .map((letter, idx) => `<span style="transition-delay:${idx * 10}ms">${letter}</span>`)
+        .map((letter, idx) => `<span style="transition-delay:${idx * 150}ms">${letter}</span>`)
         .join('')
     
 })

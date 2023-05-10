2c5a9678d6683c61afd5cacf0e9fb231495b375a
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-10 23:49:49 +0600

    Update script.js

--- Project 19 Themed Clock/script.js
+++ Project 19 Themed Clock/script.js
@@ -7,4 +7,19 @@
 
  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
 
- const months = ["January", "February", "March", "April", "", "", "", "", "", "", "", "", ];
+ const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+
+ toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if(html.classList.contains('dark')){
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+
+
+ })

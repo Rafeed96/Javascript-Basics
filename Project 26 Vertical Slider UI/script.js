7f454142db8a7ca19023f4f4fe474e78fe621138
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-15 22:46:12 +0600

    Update script.js

--- Project 26 Vertical Slider UI/script.js
+++ Project 26 Vertical Slider UI/script.js
@@ -23,5 +23,6 @@ const changeSlide = (direction) => {
     }
 
     slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 
 }

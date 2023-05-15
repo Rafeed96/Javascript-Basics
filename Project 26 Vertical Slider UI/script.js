f1b5aac1433693d0beaa66e84be97ae519002968
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-15 23:03:47 +0600

    Update script.js

--- Project 26 Vertical Slider UI/script.js
+++ Project 26 Vertical Slider UI/script.js
@@ -5,7 +5,6 @@ const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
 const slidesLength = slideRight.querySelectorAll('div').length
 
-
 let activeSlideIndex = 0
 
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
@@ -19,6 +18,11 @@ const changeSlide = (direction) => {
         activeSlideIndex++
         if(activeSlideIndex > slidesLength - 1) {
             activeSlideIndex = 0
+        }
+    }else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0){
+            activeSlideIndex = slidesLength - 1
         }
     }
 

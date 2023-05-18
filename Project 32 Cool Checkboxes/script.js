88f1de14d2518712364e6d84c478faee6c308a90
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-18 23:33:12 +0600

    Update script.js

--- Project 32 Cool Checkboxes/script.js
+++ Project 32 Cool Checkboxes/script.js
@@ -2,3 +2,27 @@ const toggles = document.querySelectorAll('toggle')
 const good = document.querySelectorAll('#good')
 const cheap = document.querySelectorAll('#cheap')
 const fast = document.querySelectorAll('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(theClickeOne) {
+    if(good.checked && cheap.checked && fast.checked) {
+        if(good === theClickeOne) {
+            fast.checked = false
+        }
+
+        if(cheap === theClickeOne) {
+            good.checked = false
+        }
+
+        if(fast === theClickeOne) {
+            good.checked = false
+        }
+
+         
+
+
+
+
+    }
+}

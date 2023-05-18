4e02ae2f91e6c42e817c057a7bd991ca0c0f35a3
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-18 21:58:24 +0600

    Update script.js

--- Project 33 Notes App/script.js
+++ Project 33 Notes App/script.js
@@ -1,6 +1,6 @@
 const addBtn = document.getElementById('add')
 
-addBtn.addEventListener('click', () => addNewNote())
+addBtn.addEventListener('click', () => addNewNote('Hello World'))
 
 function addNewNote(text = '') {
     const note = document.createElement('div')
@@ -21,6 +21,9 @@ function addNewNote(text = '') {
     const deleteBtn = note.querySelector('.delete')
     const main = note.querySelector('.main')
     const textArea = note.querySelector('textarea')
+
+    textArea.value = text
+    main.innerHTML = marked(text)
 
     deleteBtn.addEventListener('click', () => {
         note.remove()

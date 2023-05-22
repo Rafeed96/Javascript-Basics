a067cf60c8c0dcec209bb61a91b9fe4fa4898ab3
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-22 21:20:08 +0600

    Update script.js

--- Project 25 Sticky Navbar/script.js
+++ Project 25 Sticky Navbar/script.js
@@ -2,5 +2,9 @@ const nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    console.log(window.scrollY, nav.offestHeight)
+    if(window.scrollY, nav.offestHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
 }

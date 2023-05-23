6197dad2b2e92d21c6373f091c3c040e7d687624
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-23 20:41:03 +0600

    Update script.js

--- Project 27 Toast Notification/script.js
+++ Project 27 Toast Notification/script.js
@@ -13,4 +13,12 @@ button.addEventListener('click', () => createNotification())
 function createNotification() {
     const notif = document.createElement('div')
     notif.classList.add('toast')
+
+    notif.innerText = getRandomMessage()
+
+    toasts.appendChild(notif)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
 }

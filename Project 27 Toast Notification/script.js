35f46627e08f5a51486710127be3d9787476a2e7
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-23 20:39:15 +0600

    Updated

---
+++ Project 27 Toast Notification/script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
+
+const messages = [
+    'Message One',
+    'Message Two',
+    'Message Three',
+    'Message Four',
+]
+
+button.addEventListener('click', () => createNotification())
+
+function createNotification() {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+}

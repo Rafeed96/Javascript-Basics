62ba295419ad711d0eb57b32ade97bed1e041ed7
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-24 21:11:32 +0600

    Update script.js

--- Project 36 Hover Board/script.js
+++ Project 36 Hover Board/script.js
@@ -4,5 +4,25 @@ const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
 const SQUARES = 500
 
 for(let i = 0; i < SQUARES; i++) {
-    const square = document.createElement('')
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseover', () => removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    console.log(color)
+}
+
+function removeColor(element) {
+
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
 }

7130b1d7e5dced4cccefd511969a11853e426295
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-24 12:08:36 +0600

    Update script.js

--- Project 35 Image Carousel/script.js
+++ Project 35 Image Carousel/script.js
@@ -22,3 +22,14 @@ function changeImage() {
 
     imgs.style.transform = `translateX(${-idx * 500}px)`
 }
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
+})

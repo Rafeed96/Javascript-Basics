14e5637735f6de4a5bdc4533b9fc108472fefec7
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-24 12:03:32 +0600

    Update script.js

--- Project 35 Image Carousel/script.js
+++ Project 35 Image Carousel/script.js
@@ -5,3 +5,16 @@ const rightBtn = document.getElementById('right')
 const img = document.querySelectorAll('#imgs img')
 
 let idx = 0
+
+let interval = setInterval(run, 2000)
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length) {
+        
+    }
+}

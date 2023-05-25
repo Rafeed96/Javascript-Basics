f12314dbd473d1e8453bd163c6bae37f0cf3efc8
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-25 20:58:40 +0600

    Update script.js

--- Project 41 Verify Account/script.js
+++ Project 41 Verify Account/script.js
@@ -5,7 +5,10 @@ codes[0].focus()
 codes.forEach((cide, idx) => {
     code.addEventListener('keydown', (e) => {
         if(e.key >= 0 && e.key <=9) {
-            codes[idx + 1].focus()
+            codes[idx].value = ''
+            setTimeout(() => codes[idx + 1].focus(), 10)
+        } else if(e.key === 'Backspace') {
+            setTimeout(() => codes[idx - 1].focus(), 10)
         }
     })
 })

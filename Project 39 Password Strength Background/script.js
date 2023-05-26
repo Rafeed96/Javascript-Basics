c13c9304ab4c9279b7da7284526750d742ee774d
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-26 22:58:32 +0600

    Updated

--- Project 39 Password Strength Background/script.js
+++ Project 39 Password Strength Background/script.js
@@ -4,6 +4,9 @@ const background = document.getElementById('background')
 password.addEventListener('input', (e) => {
     const input = e.target.value
     const length = input.length
+    console.log(20 - length * 2)
+
     const blurValue = 20 - length * 2
+
     background.style.filter = `blue(${blurValue}px)`     
 })

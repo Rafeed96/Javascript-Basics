597aaedc770a3826bd731914518172e0136662dd
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-27 17:23:43 +0600

    Update script.js

--- Project 47 Testimonial Box Switcher/script.js
+++ Project 47 Testimonial Box Switcher/script.js
@@ -7,49 +7,40 @@ const role = document.querySelector('.role')
 const testimonials = [
     // Add photo name and images
     {
-        name:,
-        position:,
-        photo:,
+        name: 'Tim Kane',
+        position: 'Project Manager',
+        photo: 'https://www.reef2reef.com/data/avatars/h/161/161705.jpg',
+        text:'I just wanted to share a quick note and let you know that you guys do a really good job. I am glad I decided to work with you. It is really great how easy your websites are to update and manage. I never have any problem at all.'
+
+    },
+
+    {
+        name: 'Ken Cha',
+        position: 'Software Engineer',
+        photo: 'https://media.istockphoto.com/id/915669382/photo/portrait-of-young-asian-boy.jpg?s=612x612&w=0&k=20&c=GZCNkVkWXnXEh_NYpt7XuE_SGKLQnAqwOaxgTgE8d_4=',
+        text: 'Wow. I just updated my site and it was SO SIMPLE. I am blown away. You guys truly kick ass. Thanks for being so awesome. High fives!'
+    },
+
+    {
+        name: 'Tamara Jones',
+        position: 'UI/UX Designer',
+        photo: 'https://pbs.twimg.com/profile_images/506472620045262850/txfpJZG9_400x400.jpeg',
+        text: 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change and click save. Thanks, guys!'
+
+    },
+
+    {
+        name: 'Abby Lee Marting',
+        position: 'Human Resources',
+        photo: 'https://media.istockphoto.com/id/1084661126/photo/close-up-of-woman-with-blank-expression.jpg?s=170667a&w=0&k=20&c=QSwgvRNzuJoHz2YghAibK5C_mvGP7ege-K6VecXkIzQ=',
         text:,
 
     },
 
     {
-        name:,
-        position:,
-        photo:,
-        text:,
-
-    },
-
-    {
-        name:,
-        position:,
-        photo:,
-        text:,
-
-    },
-
-    {
-        name:,
-        position:,
-        photo:,
-        text:,
-
-    },
-
-    {
-        name:,
-        position:,
-        photo:,
-        text:,
-
-    },
-
-    {
-        name:,
-        position:,
-        photo:,
+        name: 'Kendra Green',
+        position: 'Sales',
+        photo: 'https://i.pinimg.com/originals/b7/74/fd/b774fd854e367926512a11fff0a5905c.jpg',
         text:,
 
     },

9ec81a9adbcff74c6dd1c85993eb4ecbb7406c62
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-29 21:33:44 +0600

    Update script.js

--- Project 46 Quiz App/script.js
+++ Project 46 Quiz App/script.js
@@ -61,3 +61,18 @@ const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+}
+
+
+

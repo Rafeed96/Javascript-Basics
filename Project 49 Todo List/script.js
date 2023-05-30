18cfbd5ffbdd32acd7a46837627a0606aedd3ba2
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-30 21:43:26 +0600

    Update script.js

--- Project 49 Todo List/script.js
+++ Project 49 Todo List/script.js
@@ -32,13 +32,15 @@ function addTodo(todo) {
         todoEl.addEventListener('click', () => {
              todoEl.classList.toggle('completed')
 
-             
+             updateLS()
         })
 
         todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault()
 
             todoEl.remove()
+
+            updateLS()
         })
 
         todosUL.appendChild(todoEl)

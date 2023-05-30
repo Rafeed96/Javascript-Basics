995f04a1fce2433f193e3432d7ed73f017fd14ba
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-05-30 21:25:55 +0600

    Update script.js

--- Project 49 Todo List/script.js
+++ Project 49 Todo List/script.js
@@ -2,6 +2,13 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
+
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -38,3 +45,4 @@ function addTodo(todo) {
         input.value = ''
     }
 }
+

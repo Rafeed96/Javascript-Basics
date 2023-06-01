e461a10f9b803f486964198ef1635031a36a5d99
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-01 11:40:59 +0600

    Update script.js

---
+++ Project 42 Live User Filter/script.js
@@ -0,0 +1,8 @@
+const result = document.getElementById('result')
+const filter = document.getElementById('filter')
+const listItems = []
+
+async function getData() {
+    const res = await fetch('https://randomuser.me/api?results-50')
+    
+}

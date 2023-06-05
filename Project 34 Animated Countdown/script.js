d59ba4b2c41939ee64e8cabb46418a51d070000b
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-05 21:43:41 +0600

    Update script.js

--- Project 34 Animated Countdown/script.js
+++ Project 34 Animated Countdown/script.js
@@ -2,3 +2,11 @@ const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
 const finalMessage = document.querySelector('.final')
 const replay = document.querySelector('#replay')
+
+runAnimation()
+
+function runAnimation() {
+    nums.forEach((num, idx) => {
+        console.log(num,idx)
+    })
+}

ff73c96b4ca5ea8242af84e88bcaf28996b19c49
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-06 23:10:26 +0600

    Update script.js

--- Project 43 Feedback UI Design/script.js
+++ Project 43 Feedback UI Design/script.js
@@ -7,7 +7,19 @@ panel.addEventListener('click', (e) => {
     if(e.target.parentNode.classList.contains('rating')) {
         removeActive()
         e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
     }
+})
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank You!</strong>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support</p>
+
+    `
 })
 
 function removeActive() {

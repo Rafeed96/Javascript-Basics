1cbbe07081dfdb240500f712cc19477ef733c20c
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-06 23:06:42 +0600

    Update script.js

--- Project 43 Feedback UI Design/script.js
+++ Project 43 Feedback UI Design/script.js
@@ -1,9 +1,17 @@
 const ratings = document.querySelectorAll('.ratings')
 const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
 
 panel.addEventListener('click', (e) => {
     if(e.target.parentNode.classList.contains('rating')) {
+        removeActive()
         e.target.parentNode.classList.add('active')
     }
 })
+
+function removeActive() {
+    for(let i = 0; i< ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
+}

67dec4af98cd2599e3a6f749e4bfbaf3ec5af181
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-07 21:40:01 +0600

    Update script.js

--- Project 16 Drinking Water/script.js
+++ Project 16 Drinking Water/script.js
@@ -3,6 +3,8 @@ const listers = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigCup()
+
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => {
         highlightCups(idx)
@@ -10,6 +12,10 @@ smallCups.forEach((cup, idx) => {
 })
 
 function highlightCups(idx) {
+    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+
     smallCups.forEach((cup, idx2) => {
         if(idx2 <= idx) {
             cup.classList.add('full')
@@ -17,4 +23,19 @@ function highlightCups(idx) {
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
 }
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+    }
+} 

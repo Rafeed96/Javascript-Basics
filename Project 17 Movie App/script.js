1464b83dfa2d65a21364c789e3c6d90bf90b35b2
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-09 20:38:05 +0600

    Update script.js

--- Project 17 Movie App/script.js
+++ Project 17 Movie App/script.js
@@ -3,7 +3,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
-
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -15,7 +15,17 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    // console.log(data.results)
+    showMovies(data.results)
+}
+
+function showMovies(movies){
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const
+    })
 }
 
 form.addEventListener('submit', (e) => {

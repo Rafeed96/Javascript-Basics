9005b14969e56de63c8aa9f6a58709e6b7339b5b
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-09 20:47:24 +0600

    Update script.js

--- Project 17 Movie App/script.js
+++ Project 17 Movie App/script.js
@@ -3,10 +3,10 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
-
 
 //get initial movies
 getMovies(API_URL)
@@ -30,7 +30,6 @@ function showMovies(movies){
 
         movieEl.innerHTML = `
         
-        <div class="movie">
         <img src="${IMG_PATH + poster_path}" alt="${title}">
         <div class="movie-info">
             <h3>${title}</h3>
@@ -42,8 +41,6 @@ function showMovies(movies){
             <h3>Overview</h3>
             ${overview}
         </div>
-    </div>
-
         `
         main.appendChild(movieEl)
 
@@ -59,7 +56,6 @@ function getClassByRate(vote) {
         return 'red'
     }
 }
-
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()

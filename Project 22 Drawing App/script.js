90e68b9082757906f137e0904d98df03e76efadb
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-09 22:34:02 +0600

    Update script.js

--- Project 22 Drawing App/script.js
+++ Project 22 Drawing App/script.js
@@ -2,7 +2,36 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20
+let isPressed = false
 let color = 'black'
+let x
+let y
+
+canvas.addEventListener("mousedown", (e) => {
+    isPressed = true
+
+    x = e.offsetX
+    y = e.offsetY
+
+})
+
+canvas.addEventListener("mouseup", (e) => {
+    isPressed = false
+
+    x = undefined
+    y = undefined
+
+})
+
+canvas.addEventListener("mousemove", (e) => {
+    if (isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2,y2)
+    }
+})
+
 
 function drawCircle(x, y) {
     ctx.beginPath();

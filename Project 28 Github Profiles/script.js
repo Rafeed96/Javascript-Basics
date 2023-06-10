32136353c29624633b2c1986453038113d073e01
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-10 21:24:59 +0600

    Update script.js

--- Project 28 Github Profiles/script.js
+++ Project 28 Github Profiles/script.js
@@ -1,1 +1,9 @@
 const APIURL = 'https://api.github.com/users/'
+
+getUser('rafeed96')
+
+function getUser(username) {
+    axios(APIURL + username)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+}

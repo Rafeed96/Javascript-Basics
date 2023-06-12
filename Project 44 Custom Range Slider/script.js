5deb92ddf366001adaecebbed00314419720ac35
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-12 13:06:34 +0600

    Update script.js

--- Project 44 Custom Range Slider/script.js
+++ Project 44 Custom Range Slider/script.js
@@ -4,7 +4,15 @@ range.addEventListener('input', (e) => {
     const value = +e.target.value
     const label = e.target.nextElementSibling   
 
+    const range_width = getComputedStyle(e.target).getPropertyValue('width')
 
-        
+    const label_width = getComputedStyle(label).getPropertyValue('width')
+
+    const num_width = +range_width.substring(0, range_width.length - 2)
+
+    const num_label_width = +label_width.substring(0, label_width.length - 2)
+
+    console.log(num_width, num_label_width)
+
     label.innerHTML = value
 })

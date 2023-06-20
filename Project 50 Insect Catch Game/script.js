a8217db275563e4302b6e9cd23b5637223a94fed
Rafeed96 <120632197+Rafeed96@users.noreply.github.com>
2023-06-20 21:38:22 +0600

    Update script.js

--- Project 50 Insect Catch Game/script.js
+++ Project 50 Insect Catch Game/script.js
@@ -5,3 +5,10 @@ const game_container = document.getElementById('game-container')
 const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
 const message = document.getElementById('message')
+
+let seconds = 0
+let score = 0
+let selected_insect = {}
+
+start_btn.addEventListener('click', () => screens[0].classList.add('up')
+)
